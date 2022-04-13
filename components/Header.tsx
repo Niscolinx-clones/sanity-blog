3027f9d5c9b39a493407bd4868f606a3fee465d0
Igboanugwo Collins <munisco12@gmail.com>
2022-04-13 17:16:24 +0100

    working on the header

---
+++ components/Header.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+import React from 'react'
+
+function Header() {
+  return (
+    <header>
+            <div>
+        <Link href="/">
+            <img src='https://links.papareact.com/yvf' alt=''/>
+        </Link>
+    </div>
+    <div></div>
+    </header>
+  )
+}
+
+export default Header
