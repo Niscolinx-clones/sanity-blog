0c2b82e0f26339db41fbc11439705ffe716fba2c
Igboanugwo Collins <munisco12@gmail.com>
2022-04-13 17:27:58 +0100

    working on header

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,12 +4,23 @@ import React from 'react'
 function Header() {
   return (
     <header>
-            <div>
-        <Link href="/">
-            <img src='https://links.papareact.com/yvf' alt=''/>
-        </Link>
-    </div>
-    <div></div>
+      <div className='flex space-x-5 items-center'>
+        <div>
+          <Link href="/">
+            <img
+              src="https://links.papareact.com/yvf"
+              alt=""
+              className="w-44 cursor-pointer object-contain"
+            />
+          </Link>
+        </div>
+        <div className='hidden md:inline-flex items-center space-x-5'>
+            <h3>About</h3>
+            <h3>Contact</h3>
+            <h3 className='text-white bg-green-600 px-4 py-1 rounded-full'>Follow</h3>
+        </div>
+        <div></div>
+      </div>
     </header>
   )
 }
