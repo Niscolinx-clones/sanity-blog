48c12887d683802539190e3b29738869e098dc9b
Igboanugwo Collins <munisco12@gmail.com>
2022-04-14 07:42:42 +0100

    working on the header

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 function Header() {
   return (
-    <header>
+    <header className='flex justify-between p-5 max-w-7xl mx-auto'>
       <div className='flex space-x-5 items-center'>
         <div>
           <Link href="/">
@@ -19,10 +19,13 @@ function Header() {
             <h3>Contact</h3>
             <h3 className='text-white bg-green-600 px-4 py-1 rounded-full'>Follow</h3>
         </div>
-        <div></div>
       </div>
+        <div className='flex items-center space-x-5 text-green-600'>
+          <h3>Sign In</h3>
+          <h3 className='border px-4 py-1 rounded-full border-green-600'>Get Started</h3>
+        </div>
     </header>
-  )
+  ) 
 }
 
 export default Header
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,24 @@ import Header from '../components/Header'
 const Home: NextPage = () => {
   return (
     <div>
-      <Header/>
+      <Head>
+        <title>Medium Blog</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+
+      <div>
+        <div>
+          <h1 className="max-w-xl font-serif text-6xl">
+            <span className='underline decoration-black decoration-4'>Medium</span> is a place to write, read, and connect
+          </h1>
+          <h2>
+            It's easy and free to post your thinking on any topic and connect
+            with millions of readers
+          </h2>
+        </div>
+        <div></div>
+      </div>
     </div>
   )
 }
