592567ff22436a036db90e0604ba0611b555b9ff
Igboanugwo Collins <munisco12@gmail.com>
2022-04-14 11:41:04 +0100

    added logos and write ups

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,8 +12,8 @@ const Home: NextPage = () => {
       </Head>
       <Header />
 
-      <div>
-        <div>
+      <div className='flex justify-between items-center bg-yellow-400 border-y border-black py-10 lg:py-0'>
+        <div className='px-10 space-y-5'>
           <h1 className="max-w-xl font-serif text-6xl">
             <span className='underline decoration-black decoration-4'>Medium</span> is a place to write, read, and connect
           </h1>
@@ -22,7 +22,7 @@ const Home: NextPage = () => {
             with millions of readers
           </h2>
         </div>
-        <div></div>
+        <img src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png" alt="" className='hidden md:inline-flex h-32 lg:h-full' />
       </div>
     </div>
   )
