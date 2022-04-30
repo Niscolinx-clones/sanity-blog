5f0ff6b24b7921dc42cfb2da7415ff329d28136c
Igboanugwo Collins <munisco12@gmail.com>
2022-04-30 14:48:50 +0100

    setting up windi css

--- containers/Trending.tsx
+++ containers/Trending.tsx
@@ -3,10 +3,10 @@ import TrendingSvg from '../assets/svgs/trending.svg'
 
 function Trending() {
   return (
-    <div className="col-start-2 col-end-18 grid bg-blue-800">
+    <div className="col-start-2 col-end-18 grid bg-blue-700">
       <header className="flex">
         <TrendingSvg />
-        <p className='font-bold text-xs'>Trending on Medium</p>
+        <p className='font-bold text-xs uppercase'>Trending on Medium</p>
       </header>
     </div>
   )
--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const path = require('path')
+const WindiCSSWebpackPlugin = require('windicss-webpack-plugin')
 
 module.exports = {
   reactStrictMode: true,
@@ -10,8 +11,9 @@ module.exports = {
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
-      use: [{loader: '@svgr/webpack', options: {icons: true}}],
+      use: [{ loader: '@svgr/webpack', options: { icons: true } }],
     })
+    config.plugins.push(new WindiCSSWebpackPlugin())
 
     return config
   },
