4813561b453d5851c6ce810b16a430853a16005e
Igboanugwo Collins <munisco12@gmail.com>
2022-05-02 15:04:58 +0100

    working on placing the background on the hero

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,14 +4,17 @@ import Header from '../components/Header'
 import Trending from '../containers/Trending'
 
 const Home: NextPage = () => {
+  const calHeight = document.querySelector('.header__height')
+
+  console.log({calHeight})
   return (
     <div className="grid-cols-18 mx-auto grid w-full max-w-7xl">
       <Head>
         <title>Medium Blog Clone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="relative contents">
-        <Header />
+      <div className="relative contents header__height">
+        <Header/>
         <div className="-z-1 absolute left-0 h-[37%] w-screen bg-[#febf17]"></div>
       </div>
       <Trending />
