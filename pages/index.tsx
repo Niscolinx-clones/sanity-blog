e2bf9161bdbf5db29ccb7a304460d4163c057946
Igboanugwo Collins <munisco12@gmail.com>
2022-05-10 05:50:15 +0100

    added query using SSR

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,9 +9,8 @@ interface Props {
   posts: [Post]
 }
 
-export default function Home({posts}: Props) {
-  console.log({posts})
-  
+const Home: NextPage<Props> = ({posts}) => {
+  console.log(posts)
   return (
     <div className="grid-cols-18  mx-auto grid w-full max-w-7xl">
       <Head>
@@ -24,6 +23,7 @@ export default function Home({posts}: Props) {
   )
 }
 
+export default Home
 
 export const getServerSideProps = async () => {
   const query = `*[_type == "post"]{
