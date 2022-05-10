155572081ddb4fa35f1526957dc13782a60000bb
Igboanugwo Collins <munisco12@gmail.com>
2022-05-10 05:10:36 +0100

    added query using SSR

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,16 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Header from '../components/Header'
 import Trending from '../containers/Trending'
+import { sanityClient } from '../sanity'
 
 const Home: NextPage = () => {
-
   return (
     <div className="grid-cols-18  mx-auto grid w-full max-w-7xl">
       <Head>
         <title>Medium Blog Clone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-        <Header/>
+      <Header />
       <Trending />
     </div>
   )
@@ -19,5 +19,18 @@ const Home: NextPage = () => {
 
 export default Home
 
+export const getServerSideProps = async () => {
+  const query = `*[_type == "post"]{
+      _id,
+      title,
+     author -> {
+        name,
+        image
+      },
+    description,
+    mainImage,
+    slug
+  }`
 
-
+  const posts = await sanityClient.fetch(query)
+}
