d13d171406f6ed8f23b7aed34a50522d5d738213
Igboanugwo Collins <munisco12@gmail.com>
2022-05-10 07:25:54 +0100

    added query using SSR

--- components/Trending.tsx
+++ components/Trending.tsx
@@ -4,25 +4,25 @@ import TrendingSvg from '../assets/svgs/trending.svg'
 import { urlFor } from '../sanity'
 import { PostProps } from '../typings'
 
-function Trending({posts}: PostProps) {
+function Trending({ posts }: PostProps) {
   return (
-    <div className="col-end-18 col-start-2 grid">
-      <header className="flex">
+    <div className="col-end-18 col-start-2 grid mt-10">
+      <header className="flex items-center mb-3">
         <TrendingSvg />
-        <p className="text-xs font-bold uppercase">Trending on Medium</p>
-        <div className="pody">
-          {posts.map((post) => {
-            return (
-              <Link href={`/posts/${post.slug.current}`} key={post._id}>
-                <>
-                  <p>{post.title}</p>
-                  <img src={urlFor(post.author.image).url()} alt="" />
-                </>
-              </Link>
-            )
-          })}
-        </div>
+        <p className="text-xs font-bold uppercase ml-2">Trending on Medium</p>
       </header>
+      <div className="pody">
+        {posts.map((post) => {
+          return (
+            <Link href={`/posts/${post.slug.current}`} key={post._id}>
+              <>
+                <p>{post.title}</p>
+                <img src={urlFor(post.author.image).url()} alt="" />
+              </>
+            </Link>
+          )
+        })}
+      </div>
     </div>
   )
 }
