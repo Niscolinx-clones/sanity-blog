102546af8a97ea575bea5ff0fe24f8a090d4b5f5
Igboanugwo Collins <munisco12@gmail.com>
2022-05-12 18:56:10 +0100

    added post slug page

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { sanityClient } from '../../sanity'
 
 export default function Post() {
-  return (
-    <div>[slug]</div>
-  )
+  return <div>[slug]</div>
+}
+
+export const getStaticPaths = async () => {
+  const query = 
+    await sanityClient.fetch(`*[_type == "post" && slug.current == $slug][0]{
+      _id,
+    slug
+  }`)
 }
