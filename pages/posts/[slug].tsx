0e9d5c555e922934d1dcbc508c595e973d3046eb
Igboanugwo Collins <munisco12@gmail.com>
2022-05-15 07:13:42 +0100

    added ssr

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -4,92 +4,54 @@ import React from 'react'
 import { sanityClient } from '../../sanity'
 import { Post } from '../../typings'
 
-export default function Slug({post}: {post: Post}) {
+export default function Slug(props: any) {
 
-  return <div>{post.title}</div>
+  return <div></div>
 }
 
-// export const getStaticPaths: GetStaticPaths = async () => {
-//   const query = `*[_type == "post"]{
-//       _id,
-//     slug {
-//         current
-//     }
-//   }`
-//   const posts = await sanityClient.fetch(query)
+export const getStaticPaths: GetStaticPaths = async () => {
+  const query = `*[_type == "post"]{
+      _id,
+    slug {
+        current
+    }
+  }`
+  const posts = await sanityClient.fetch(query)
 
-//   console.log('posts', posts)
-//   const paths = posts.map((post: Post) => ({
-//     params: {
-//       slug: post.slug.current,
-//     },
-//   }))
+  console.log('posts', posts)
+  const paths = posts.map((post: Post) => ({
+    params: {
+      slug: post.slug.current,
+    },
+  }))
 
-//   console.log({ paths })
-//   return {
-//     paths,
-//     fallback: 'blocking',
-//   }
-// }
+  console.log({ paths })
+  return {
+    paths,
+    fallback: 'blocking',
+  }
+}
 
-// export const getStaticProps: GetStaticProps = async ({ params }) => {
-//   const query = `*[_type == "post" && slug.current == $slug][0]{
-//       _id,
-//   _createdAt,
-//   title,
-//   body,
-//     slug {
-//   current
-// },
-// description,
-// author -> {
-//   name,
-//   image
-// },
-// mainImage,
-// 'comments': *[
-//   _type == "comment" && post._ref == ^._id && approved == true]
-
-//   }`
-
-//   const post = await sanityClient.fetch(query, {
-//     slug: params?.slug,
-//   })
-
-//   if (!post) {
-//     return {
-//       notFound: true,
-//     }
-//   }
-
-//   return {
-//     props: {
-//       post,
-//     },
-//   }
-// }
-
-export const getServerSideProps: GetServerSideProps = async ({ params }) => {
-  console.log('props for server', params?.slug)
-
+export const getStaticProps: GetStaticProps = async ({ params }) => {
   const query = `*[_type == "post" && slug.current == $slug][0]{
-        _id,
-    _createdAt,
-    title,
-    body,
-      slug {
-    current
-  },
-  description,
-  author -> {
-    name,
-    image
-  },
-  mainImage,
-  'comments': *[
-    _type == "comment" && post._ref == ^._id && approved == true]
+      _id,
+  _createdAt,
+  title,
+  body,
+    slug {
+  current
+},
+description,
+author -> {
+  name,
+  image
+},
+mainImage,
+'comments': *[
+  _type == "comment" && post._ref == ^._id && approved == true]
 
-    }`
+
+  }`
 
   const post = await sanityClient.fetch(query, {
     slug: params?.slug,
@@ -107,3 +69,6 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
     },
   }
 }
+
+
+
