566205106b0a08dc87e936c636eee112d5c64cf3
Igboanugwo Collins <munisco12@gmail.com>
2022-05-16 06:04:09 +0100

    added revalidation

--- components/Header.tsx
+++ components/Header.tsx
@@ -24,7 +24,7 @@ function Header() {
 
   return (
     <header className="grid-cols-18 header__height col-span-full grid">
-      <div className="-z-1 absolute left-0 w-screen bg-[#febf17]" style={{height}}></div>
+      <div className="-z-1 absolute left-0 w-screen bg-[#febf17] border-b border-black" style={{height}}></div>
 
       <div className="col-end-18 col-start-2 grid">
         <div className="flex items-center justify-between py-5">
