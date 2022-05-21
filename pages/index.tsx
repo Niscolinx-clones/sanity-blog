28794bb4a7afaf913798c94bf45831596ec1b813
Igboanugwo Collins <munisco12@gmail.com>
2022-05-21 08:57:29 +0100

    added images for post and authors

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,22 +6,6 @@ import Trending from '../components/Trending'
 import { sanityClient } from '../sanity'
 import { PostProps } from '../typings'
 
-const Home: NextPage<PostProps> = ({ posts }) => {
-  return (
-    <>
-      <Head>
-        <title>Medium Blog Clone</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Header />
-      <Trending posts={posts} />
-      <Trending posts={posts} />
-      {/* <Posts posts={posts} /> */}
-    </>
-  )
-}
-
-export default Home
 
 export const getServerSideProps = async () => {
   const query = `*[_type == "post"]{
@@ -46,3 +30,20 @@ export const getServerSideProps = async () => {
     },
   }
 }
+
+const Home: NextPage<PostProps> = ({ posts }) => {
+  return (
+    <>
+      <Head>
+        <title>Medium Blog Clone</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      <Trending posts={posts} />
+      {/* <Posts posts={posts} /> */}
+    </>
+  )
+}
+
+export default Home
+
