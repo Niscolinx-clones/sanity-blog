7c4971fc30eff5d5fb9737f4d0fe01682f6d9966
Igboanugwo Collins <munisco12@gmail.com>
2022-05-24 00:33:54 +0100

    styling categories

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -4,14 +4,14 @@ import { categoryQuery } from '../typings'
 function Categories({ categoryQuery }: categoryQuery) {
   return (
     <div>
-      <p className="text-xs font-bold uppercase">
+      <p className="text-xs font-bold uppercase mb-4">
         Discover more of what matters to you
       </p>
 
-      <ul className="flex list-none flex-wrap gap-y-2 max-w-[20rem]">
+      <ul className="flex list-none flex-wrap gap-2 max-w-[25rem]">
         {categoryQuery.map((category, i) => {
           return (
-            <li key={i} className="border p-2 flex text-xs text-gray-600">
+            <li key={i} className="border p-2 flex text-xs text-gray-600 space-x-2">
               {category.title}
             </li>
           )
