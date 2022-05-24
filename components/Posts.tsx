99921e0248db87aab04a7a7ad8bbe026947d72a0
Igboanugwo Collins <munisco12@gmail.com>
2022-05-24 01:53:26 +0100

    styling categories

--- components/Posts.tsx
+++ components/Posts.tsx
@@ -9,33 +9,36 @@ export default function Posts({ posts }: PostProps) {
     <div className="">
       {posts.map((post) => {
         return (
-          <div className="grid gap-1">
-            <Link href={`/post/${post.slug.current}`}>
-              <div className="contents cursor-pointer">
-                <div className="flex gap-2">
-                  <img
-                    src={urlFor(post.author.image).url()}
-                    alt="author picture"
-                    className="h-6 w-6 rounded-full object-cover"
-                  />
-                  <h3 className="text-sm font-medium">{post.author.name}</h3>
+          <div className="flex gap-1">
+            <div className="grid gap-1">
+              <Link href={`/post/${post.slug.current}`}>
+                <div className="contents cursor-pointer">
+                  <div className="flex gap-2">
+                    <img
+                      src={urlFor(post.author.image).url()}
+                      alt="author picture"
+                      className="h-6 w-6 rounded-full object-cover"
+                    />
+                    <h3 className="text-sm font-medium">{post.author.name}</h3>
+                  </div>
+                  <p className="text-lg font-bold">{post.title}</p>
                 </div>
-                <p className="text-lg font-bold">{post.title}</p>
+              </Link>
+              <div className="flex items-center text-[#757575]">
+                <p>
+                  {new Date(post.publishedAt).toLocaleString('en-US', {
+                    day: '2-digit',
+                    month: 'short',
+                  })}
+                </p>
+                &#1793; <span>{post.readTime} min read ܁</span>{' '}
+                <span className="rounded-full bg-gray-100 p-2">
+                  {post.category[0].title}
+                </span>
+                <BookMarkSvg />
               </div>
-            </Link>
-            <div className="flex items-center text-[#757575]">
-              <p>
-                {new Date(post.publishedAt).toLocaleString('en-US', {
-                  day: '2-digit',
-                  month: 'short',
-                })}
-              </p>
-              &#1793; <span>{post.readTime} min read ܁</span>{' '}
-              <span className="rounded-full bg-gray-100 p-2">
-                {post.category[0].title}
-              </span>
-              <BookMarkSvg />
             </div>
+            <img src={urlFor(post.mainImage).url()} alt=''/>
           </div>
         )
       })}
