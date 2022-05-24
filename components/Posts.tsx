b9ddeb5582ec0f622a0b3f038131f53e89b39b47
Igboanugwo Collins <munisco12@gmail.com>
2022-05-24 14:06:06 +0100

    added portable text

--- components/Posts.tsx
+++ components/Posts.tsx
@@ -6,7 +6,7 @@ import { PortableText } from '@portabletext/react'
 
 
 export default function Posts({ posts }: PostProps) {
-  console.log('posts', posts[0].body)
+  console.log('posts', posts)
   const myPortableTextComponents = {
     types: {
       image: ({ value }:any) => <img src={urlFor(value).url()} />,
@@ -62,7 +62,7 @@ export default function Posts({ posts }: PostProps) {
                   {post.category[0].title}
                 </span>
                 <span>
-                  <PortableText value={post.body[0]} components={myPortableTextComponents}/>
+                  <PortableText value={post.body[0] ? post.body[0] : post.body[1]} components={myPortableTextComponents}/>
                 </span>
                 <div className="cursor-pointer ml-auto">
 
